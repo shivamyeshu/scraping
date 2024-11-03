8a42e120e95b5852799ddf1b98c38977b0438ee7
Shivam <145973348+shivamyeshu@users.noreply.github.com>
2024-11-04 00:19:37 +0530

    init

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Happy developing ✨')
+const fs = require("fs");
+
+console.log("here mwe go again before ")
+//
+// let data = fs.readFileSync("a.txt", "utf8");
+// console.log(data);
+
+//async fctn.
+let data = fs.readFile("a.txt", "utf-8" , cb )
+function cb(err , data ){
+    console.log("the data of a.txt is = " + data)
+    console.log("end")
+}
+
+
+console.log("this is after the complition ")
+
+
